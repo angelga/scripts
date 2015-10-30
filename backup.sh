2159f525da23a06331b4ae4887cee7de2d6abdf2
Angel Garcia <azmavet@gmail.com>
2015-10-29 18:28:51 -0600

    Update backup script for using with mac

--- backup.sh
+++ backup.sh
@@ -2,6 +2,7 @@
 #
 # Backup personal files
 #
+# find . -depth -mindepth 1 -maxdepth 1 -exec rm -rf {} \;
 
 if [ "$#" -ne 1 ]; then
 	echo "Usage: backup.sh destination_path"
@@ -13,10 +14,33 @@ if [ ! -d "$1" ]; then
 	exit 1
 fi
 
-echo "Archiving to $1"
+# expand destination path
+DEST_PATH=`cd "$1"; pwd`
+HOME_PATH=`cd ~; pwd`
 
-rsync -av ~/.password-store "$1"
-rsync -av ~/.inputrc "$1"
-rsync -av ~/.gnupg "$1"
-rsync -av ~/.profile "$1"
-rsync -av ~/.ssh "$1"
+if [ "$DEST_PATH" = "$HOME_PATH" ]; then
+	echo "Destination cannot be the home directory"
+	exit 1
+fi
+
+echo "Archiving to $DEST_PATH"
+pushd ~
+
+echo "Archiving Keychains"
+sudo cp "/Library/Keychains/System.keychain" "/var/db/SystemKey" $DEST_PATH
+sudo chown $USER "$DEST_PATH/System.keychain" "$DEST_PATH/SystemKey"
+
+sudo find /Users -type d -mindepth 1 -maxdepth 1 -not -iname "shared" -exec sh -c \
+	'dirname_only=`basename "$0"`; mkdir "$1"/$dirname_only; \
+	cp "$0"/Library/Keychains/*.keychain "$1"/$dirname_only; \
+	chown -R "$2" "$1"/$dirname_only' \
+	{} $DEST_PATH $USER \;
+
+echo "Archiving dot files"
+#TODO don't overwrite if existing
+rsync -a ~/.bash_profile "$DEST_PATH"
+rsync -a ~/.inputrc "$DEST_PATH"
+rsync -a ~/.ssh "$DEST_PATH"
+
+echo "Done"
+popd
